ab059c2d2a7322a4e137cc5f79b012ec802e4d0d
stephentetley <stephen.tetley@gmail.com>
2022-03-16 21:08:46 +0000

    Removing Java wrappers as they weren't being used.

--- src-java/make-jar.bat
+++
@@ -1,9 +0,0 @@
-
-set %JAVA_HOME%="C:\Program Files\Zulu\zulu-11"
-
-javac -cp "../lib/commons-dbutils-1.7.jar;../lib/duckdb_jdbc-0.2.1.jar;../lib/sqlite-jdbc-3.36.0.3.jar" -d ./build  flixspt/flixbasicdb/*.java
-
-cd build 
-
-jar cf flix-basicdbjava-1.0.jar ./flixspt/flixbasicdb/*.class
-
